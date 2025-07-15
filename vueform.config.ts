d37c26adb3d254245025d23944bb0fda0fae4ca8
novakgyorgytibor <novak.gyorgy.tibor@gmail.com>
2025-07-15 17:51:30 +0200

    [fix] Cannot find module

--- vueform.config.ts
+++ vueform.config.ts
@@ -1,5 +1,8 @@
+// @ts-ignore
 import en from "@vueform/vueform/locales/en";
+// @ts-ignore
 import tailwind from "@vueform/vueform/dist/tailwind";
+// @ts-ignore
 import { defineConfig } from "@vueform/vueform";
 
 export default defineConfig({
